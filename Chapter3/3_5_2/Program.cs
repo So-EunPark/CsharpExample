e6106c23598707c58d407d6aa9083dc670c0c20e
soeun <70000city@gmail.com>
2019-12-16 00:15:36 +0900

    3장

--- Chapter3/3_5_2/Program.cs
+++ Chapter3/3_5_2/Program.cs
@@ -8,6 +8,7 @@ namespace _3_5_2
 {
     class Program
     {
+        enum DialogResult {YES, NO, CANCLE, CONFIRM, OK}
         static void Main(string[] args)
         {
 
---
+++ Chapter3/3_6Nullable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_6Nullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? a = null;
+
+            Console.WriteLine(a.HasValue); //값을 갖고있는가? -> false출력
+            Console.WriteLine(a != null);  //null이 아닌가? -> false출력(null이기 때문에)
+
+            a = 3;
+
+            Console.WriteLine(a.HasValue);
+            Console.WriteLine(a != null);
+            Console.WriteLine(a);
+        }
+    }
+}
