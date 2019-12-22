98c9603475fec3e822d30acd8b13608fbdcf1d5d
soeun <70000city@gmail.com>
2019-12-23 00:26:15 +0900

    ch4,5

---
+++ DB박소은_프로젝트작성1/박소은_프로젝트1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.Collections;
+
+
+namespace 박소은_프로젝트1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.Write("행 수를 입력하세요. : ");
+            num = int.Parse(Console.ReadLine());
+
+            if (num < 0)
+            {
+                Console.WriteLine("\n0보다 작거나 같은 숫자는 입력할 수 없습니다.\n");
+                Environment.Exit(0); //프로그램 종료
+
+            }
+
+            for (int i = 0; i < num; i++)
+            {
+                for (int j = 0 ; j<num-i-1 ; j++) //공백을 찍어준다
+                    Console.Write(" ");
+                
+                for(int j=0; j<(2*i)+1 ; j++) // 별찍기
+                    Console.Write("*");
+
+                Console.WriteLine();//줄넘기기
+            }
+          
+        }
+    }
+}
---
+++ DB박소은_프로젝트작성1/박소은_프로젝트1_2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 박소은_프로젝트1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int kor, eng, math, scty, sci, total;
+            double avg;
+            string gp;
+
+            Console.Write("국어점수를 입력하시오. (0~100사이의 정수를 입력하세요): ");
+            kor = int.Parse(Console.ReadLine());
+            Console.Write("영어점수를 입력하시오. (0~100사이의 정수를 입력하세요): ");
+            eng = int.Parse(Console.ReadLine());
+            Console.Write("수학점수를 입력하시오. (0~100사이의 정수를 입력하세요): ");
+            math = int.Parse(Console.ReadLine());
+            Console.Write("사회점수를 입력하시오. (0~100사이의 정수를 입력하세요): ");
+            scty = int.Parse(Console.ReadLine());
+            Console.Write("과학점수를 입력하시오. (0~100사이의 정수를 입력하세요): ");
+            sci = int.Parse(Console.ReadLine());
+
+            total = kor + eng + math + scty + sci;
+            avg = total / 5.0;
+
+            switch ((int)avg/10)
+            {
+                case 10:
+                    gp="A+";
+                    break;
+                case 9:
+                    gp = "A";
+                    break;
+                case 8:
+                    gp = "B";
+                    break;
+                case 7:
+                    gp = "C";
+                    break;
+                case 6:
+                    gp = "D";
+                    break;
+                default:
+                    gp = "F";
+                    break;
+            }
+
+            Console.WriteLine("평균점수:{0}\t 학점:{1}\t",avg, gp);
+
+        }
+    }
+}
